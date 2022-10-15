5b26fec79d18060f2bb25eaa97f48d9c074076ae
ZARIFMAC <uzbzarif@gmail.com>
2022-10-15 16:16:58 +0500

    read json from file

--- bin/grocery_store.dart
+++ bin/grocery_store.dart
@@ -1,3 +1,11 @@
+import 'dart:convert';
+import 'dart:io';
+
 void main() {
-  print('hello');
+//  Read the file
+  var file = File('grocery.json');
+  var contents = file.readAsStringSync();
+
+  // Convert the contents to a map
+  var groceryMap = jsonDecode(contents);
 }
